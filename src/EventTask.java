5ebecc0f9cedefbeb8c8df3cdcbaee5b382629f3
tolspy98 <rubensanchezg2001@gmail.com>
2024-12-11 23:11:21 +0100

    Initial commit

---
+++ src/EventTask.java
@@ -0,0 +1,13 @@
+public class EventTask {
+    private String text;
+    private boolean isCompleted;
+
+    public EventTask(String text) {
+        this.text = text;
+        this.isCompleted = false;
+    }
+    public void toggleCompleted() {
+        isCompleted = !isCompleted;
+    }
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Main programa = new Main();
+        programa.inicio();
+    }
+
+    public void inicio() {
+
+
+    }
+}
